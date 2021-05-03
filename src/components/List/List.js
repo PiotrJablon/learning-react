c0a85ca1174e98e935c7ae090d0180775498902b
Piotrek <pjablonski123.00@wp.pl>
2021-05-03 22:48:10 +0200

    fix bug

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,7 +6,12 @@ import Hero from '../Hero/Hero.js';
 class List extends React.Component {
   render() {
     return (
-      <Hero />
+      <section>
+        <Hero titleText={this.props.title} />
+        <div className={styles.description}>
+  		  {this.props.children}
+		</div>
+      </section>
     )
   }
 }
